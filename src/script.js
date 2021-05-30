20121fd3c9750b9fa0d57588a988d0afd4c807a1
Nikson Rotondaro <nikson_rotondaro@hotmail.com>
2021-05-29 23:12:08 -0300

    feat(home): adding content and style

---
+++ src/script.js
@@ -0,0 +1,28 @@
+$(document).ready( () => {
+    const slickOptions = {
+        autoplay: true,
+        autoplaySpeed: 4500,
+        dots: false,
+        prevArrow: '<button type="button" class="slick-prev">Previous</button>;',
+        nextArrow: '<button type="button" class="slick-next">Previous</button>;'
+    };
+    $('.slider').slick(slickOptions);
+
+    $('.footer__form-button').on('click', () => {
+        const email = $('#email').val();
+        Email.send({
+            Host : "smtp.elasticemail.com",
+            Username : "niksonrotondaro1@gmail.com",
+            Password : "DC4049BF23107FAEC385E815EB67D69D3E74",
+            To : 'nikson_rotondaro@hotmail.com',
+            From : email,
+            Subject : "Please add me to the newsletter",
+            Body : `Olá. 
+            Eu gostaria de ser adicionado na newsletter do site. 
+            Meu email é ${email}. 
+            Obrigado!`
+        }).then(
+          message => alert(message)
+        );
+    })
+});
